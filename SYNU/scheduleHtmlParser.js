4a26fd19db61717fedd141d82c1fe2f0c7a68352
YuelinWang <836473734@qq.com>
2020-09-20 21:08:39 +0800

    read

--- SYNU/scheduleHtmlParser.js
+++ SYNU/scheduleHtmlParser.js
@@ -1,297 +1,37 @@
 function scheduleHtmlParser(html) {
-    //除函数名外都可编辑
-    //传入的参数为上一步函数获取到的html
-    //可使用正则匹配
-    //可使用解析dom匹配，工具内置了$，跟jquery使用方法一样，直接用就可以了，参考：https://juejin.im/post/5ea131f76fb9a03c8122d6b9
-    //以下为示例，您可以完全重写或在此基础上更改
-    //大一
-    const wyl_startTime1 = ["08:00", "08:50", "09:45", "10:35", "13:00", "13:50", "14:45", "15:35", "18:30", "19:20"]
-    const wyl_endTime1 = ["08:45", "09:35", "10:30", "11:20", "13:45", "14:35", "15:30", "16:20", "18:30", "19:20"]
+    let frg2089_result = { courseInfos: [] };
+    const frg2089_Unknown = 'Unknown';
 
-    //大二
-    const wyl_startTime2 = ["08:10", "09:00", "10:00", "10:50", "13:10", "14:00", "15:00", "15:50", "18:30", "19:20"]
-    const wyl_endTime2 = ["08:55", "09:45", "10:45", "11:35", "13:55", "14:45", "15:45", "16:35", "18:30", "19:20"]
+    let frg2089_data = JSON.parse(html.replace('<div>', '').replace('</div>', ''));
+    for (let frg2089_i = 0; frg2089_i < frg2089_data.length; frg2089_i++) {
+        let frg2089_raw = frg2089_data[frg2089_i];
+        let frg2089_re = { weeks: [] };
 
-    //大三
-    const wyl_startTime3 = ["08:20", "09:10", "10:15", "11:05", "13:20", "14:10", "15:15", "16:05", "18:30", "19:20"]
-    const wyl_endTime3 = ["09:05", "09:55", "11:00", "11:50", "14:05", "14:55", "16:00", "16:50", "18:30", "19:20"]
+        frg2089_re.name = frg2089_raw.name;
+        frg2089_re.position = frg2089_raw.place;
+        frg2089_re.teacher = frg2089_raw.teacher;
 
-    //大四
-    const wyl_startTime4 = ["08:30", "09:20", "10:30", "11:20", "13:30", "14:20", "15:30", "16:20", "18:30", "19:20"]
-    const wyl_endTime4 = ["09:15", "10:05", "11:15", "12:05", "14:15", "15:05", "16:15", "17:05", "18:30", "19:20"]
-
-    //算的头疼，没觉得code能力变强，倒是觉得心算能力增强了
-
-    let result = []
-    let bbb = $('#table1 .timetable_con')
-    let wyl_id = $('.timetable_title h6')[1].children[0].data.split('：')[1]
-    for (let u = 0; u < bbb.length; u++) {
-        let re = { sections: [], weeks: [] }
-        let aaa = $(bbb[u]).find('span')
-        let week = $(bbb[u]).parent('td')[0].attribs.id
-        if (week) {
-            re.day = week.split('-')[0]
+        if (frg2089_re.position == 'undefined' || !frg2089_re.position) {
+            frg2089_re.position = frg2089_Unknown;
         }
-        for (let i = 0; i < aaa.length; i++) {
 
-            if (aaa[i].attribs.title == '上课地点') {
-
-                for (let j = 0; j < $(aaa[i]).next()[0].children.length; j++) {
-                    re.position = $(aaa[i]).next()[0].children[j].data
-                }
-            }
-            if (aaa[i].attribs.title == '节/周') {
-
-                for (let j = 0; j < $(aaa[i]).next()[0].children.length; j++) {
-
-                    let lesson = $(aaa[i]).next()[0].children[j].data
-                    for (let a = Number(lesson.split(')')[0].split('(')[1].split('-')[0]); a < Number(lesson.split(')')[0].split('(')[1].split('-')[1].split('节')[0]) + 1; a++) {
-
-                        re.sections.push({ section: a })
-                    }
-                    for (let a = Number(lesson.split(')')[1].split('-')[0]); a < Number(lesson.split(')')[1].split('-')[1].split('周')[0]) + 1; a++) {
-
-                        re.weeks.push(a)
-                    }
-                }
-            }
-
-            if (aaa[i].attribs.title == '教师') {
-
-                for (let j = 0; j < $(aaa[i]).next()[0].children.length; j++) {
-                    re.teacher = $(aaa[i]).next()[0].children[j].data
-                }
-            }
-
-            if (aaa[i].attribs.class == 'title') {
-
-                for (let j = 0; j < $(aaa[i]).children()[0].children.length; j++) {
-                    re.name = $(aaa[i]).children()[0].children[j].data
-
-                }
-            }
-
+        let frg2089_weeks = frg2089_raw.time.match("第\\d+-\\d+周")[0].replace("第", "").replace("周", "").split('-');
+        for (let frg2089_i = Number(frg2089_weeks[0]); frg2089_i <= Number(frg2089_weeks[1]); frg2089_i++) frg2089_re.weeks.push(Number(frg2089_i));
+        let frg2089_sections = frg2089_raw.time.match("第\\d+,\\d+节");
+        if (frg2089_sections != null) {
+            frg2089_re.sections = new Array();
+            for (let frg2089_i = Number(frg2089_sections[0].replace("第", "").replace("节", "").split(',')[0]); frg2089_i <= Number(frg2089_sections[0].replace("第", "").replace("节", "").split(',')[1]); frg2089_i++) frg2089_re.sections.push(Number(frg2089_i));
         }
-        result.push(re)
-    }
-    console.log(result)
-    console.log(wyl_id)
-    let wyl_sectionTime
-    if (wyl_id[0] == 1) {
-        if (wyl_id[1] == 7) {
-            wyl_sectionTime = [{
-                    "section": 1,
-                    "wyl_startTime": wyl_startTime4[0],
-                    "wyl_endTime": wyl_endTime4[0]
-                },
-                {
-                    "section": 2,
-                    "wyl_startTime": wyl_startTime4[1],
-                    "wyl_endTime": wyl_endTime4[1]
-                },
-                {
-                    "section": 3,
-                    "wyl_startTime": wyl_startTime4[2],
-                    "wyl_endTime": wyl_endTime4[2]
-                },
-                {
-                    "section": 4,
-                    "wyl_startTime": wyl_startTime4[3],
-                    "wyl_endTime": wyl_endTime4[3]
-                },
-                {
-                    "section": 5,
-                    "wyl_startTime": wyl_startTime4[4],
-                    "wyl_endTime": wyl_endTime4[4]
-                },
-                {
-                    "section": 6,
-                    "wyl_startTime": wyl_startTime4[5],
-                    "wyl_endTime": wyl_endTime4[5]
-                },
-                {
-                    "section": 7,
-                    "wyl_startTime": wyl_startTime4[6],
-                    "wyl_endTime": wyl_endTime4[6]
-                },
-                {
-                    "section": 8,
-                    "wyl_startTime": wyl_startTime4[7],
-                    "wyl_endTime": wyl_endTime4[7]
-                },
-                {
-                    "section": 9,
-                    "wyl_startTime": wyl_startTime4[8],
-                    "wyl_endTime": wyl_endTime4[8]
-                },
-                {
-                    "section": 10,
-                    "wyl_startTime": wyl_startTime4[9],
-                    "wyl_endTime": wyl_endTime4[9]
-                }
-            ];
-        } else if (wyl_id[1] == 8) {
-            wyl_sectionTime = [{
-                    "section": 1,
-                    "wyl_startTime": wyl_startTime3[0],
-                    "wyl_endTime": wyl_endTime3[0]
-                },
-                {
-                    "section": 2,
-                    "wyl_startTime": wyl_startTime3[1],
-                    "wyl_endTime": wyl_endTime3[1]
-                },
-                {
-                    "section": 3,
-                    "wyl_startTime": wyl_startTime3[2],
-                    "wyl_endTime": wyl_endTime3[2]
-                },
-                {
-                    "section": 4,
-                    "wyl_startTime": wyl_startTime3[3],
-                    "wyl_endTime": wyl_endTime3[3]
-                },
-                {
-                    "section": 5,
-                    "wyl_startTime": wyl_startTime3[4],
-                    "wyl_endTime": wyl_endTime3[4]
-                },
-                {
-                    "section": 6,
-                    "wyl_startTime": wyl_startTime3[5],
-                    "wyl_endTime": wyl_endTime3[5]
-                },
-                {
-                    "section": 7,
-                    "wyl_startTime": wyl_startTime3[6],
-                    "wyl_endTime": wyl_endTime3[6]
-                },
-                {
-                    "section": 8,
-                    "wyl_startTime": wyl_startTime3[7],
-                    "wyl_endTime": wyl_endTime3[7]
-                },
-                {
-                    "section": 9,
-                    "wyl_startTime": wyl_startTime3[8],
-                    "wyl_endTime": wyl_endTime3[8]
-                },
-                {
-                    "section": 10,
-                    "wyl_startTime": wyl_startTime3[9],
-                    "wyl_endTime": wyl_endTime3[9]
-                }
-            ];
-        } else if (wyl_id[1] == 9) {
-            wyl_sectionTime = [{
-                    "section": 1,
-                    "wyl_startTime": wyl_startTime2[0],
-                    "wyl_endTime": wyl_endTime2[0]
-                },
-                {
-                    "section": 2,
-                    "wyl_startTime": wyl_startTime2[1],
-                    "wyl_endTime": wyl_endTime2[1]
-                },
-                {
-                    "section": 3,
-                    "wyl_startTime": wyl_startTime2[2],
-                    "wyl_endTime": wyl_endTime2[2]
-                },
-                {
-                    "section": 4,
-                    "wyl_startTime": wyl_startTime2[3],
-                    "wyl_endTime": wyl_endTime2[3]
-                },
-                {
-                    "section": 5,
-                    "wyl_startTime": wyl_startTime2[4],
-                    "wyl_endTime": wyl_endTime2[4]
-                },
-                {
-                    "section": 6,
-                    "wyl_startTime": wyl_startTime2[5],
-                    "wyl_endTime": wyl_endTime2[5]
-                },
-                {
-                    "section": 7,
-                    "wyl_startTime": wyl_startTime2[6],
-                    "wyl_endTime": wyl_endTime2[6]
-                },
-                {
-                    "section": 8,
-                    "wyl_startTime": wyl_startTime2[7],
-                    "wyl_endTime": wyl_endTime2[7]
-                },
-                {
-                    "section": 9,
-                    "wyl_startTime": wyl_startTime2[8],
-                    "wyl_endTime": wyl_endTime2[8]
-                },
-                {
-                    "section": 10,
-                    "wyl_startTime": wyl_startTime2[9],
-                    "wyl_endTime": wyl_endTime2[9]
-                }
-            ];
-        }
-    } else if (wyl_id[0] == 2) {
-        if (wyl_id[1] == 0) {
-            wyl_sectionTime = [{
-                    "section": 1,
-                    "wyl_startTime": wyl_startTime1[0],
-                    "wyl_endTime": wyl_endTime1[0]
-                },
-                {
-                    "section": 2,
-                    "wyl_startTime": wyl_startTime1[1],
-                    "wyl_endTime": wyl_endTime1[1]
-                },
-                {
-                    "section": 3,
-                    "wyl_startTime": wyl_startTime1[2],
-                    "wyl_endTime": wyl_endTime1[2]
-                },
-                {
-                    "section": 4,
-                    "wyl_startTime": wyl_startTime1[3],
-                    "wyl_endTime": wyl_endTime1[3]
-                },
-                {
-                    "section": 5,
-                    "wyl_startTime": wyl_startTime1[4],
-                    "wyl_endTime": wyl_endTime1[4]
-                },
-                {
-                    "section": 6,
-                    "wyl_startTime": wyl_startTime1[5],
-                    "wyl_endTime": wyl_endTime1[5]
-                },
-                {
-                    "section": 7,
-                    "wyl_startTime": wyl_startTime1[6],
-                    "wyl_endTime": wyl_endTime1[6]
-                },
-                {
-                    "section": 8,
-                    "wyl_startTime": wyl_startTime1[7],
-                    "wyl_endTime": wyl_endTime1[7]
-                },
-                {
-                    "section": 9,
-                    "wyl_startTime": wyl_startTime1[8],
-                    "wyl_endTime": wyl_endTime1[8]
-                },
-                {
-                    "section": 10,
-                    "wyl_startTime": wyl_startTime1[9],
-                    "wyl_endTime": wyl_endTime1[9]
-                }
-            ];
+
+        frg2089_re.day = frg2089_raw.time.substr(0, 2);
+        if (frg2089_re.day.match('周.') == null) frg2089_re.day = '周日';
+
+        if (frg2089_result.courseInfos.length > 0 && frg2089_result.courseInfos[frg2089_result.courseInfos.length - 1].name === frg2089_re.name && frg2089_result.courseInfos[frg2089_result.courseInfos.length - 1].day === frg2089_re.day) {
+            frg2089_re.weeks.forEach(i => frg2089_result.courseInfos[frg2089_result.courseInfos.length - 1].weeks.push(i));
+        } else {
+            frg2089_result.courseInfos.push(frg2089_re);
         }
     }
-    return {
-        courseInfos: result,
-        sectionTimes: wyl_sectionTime
-    }
+    console.info(frg2089_result);
+    return frg2089_result;
 }
--- SYNU/scheduleHtmlProvider.js
+++ SYNU/scheduleHtmlProvider.js
@@ -1,24 +1,27 @@
 function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom = document) {
-    //除函数名外都可编辑
-    //以下为示例，您可以完全重写或在此基础上更改
+    let frg2089_courses = "";
+    let frg2089_allItem = dom.getElementById('iframeautoheight').contentDocument.body.getElementsByTagName('table')[1].getElementsByTagName('td');
+    for (let frg2089_i = 0; frg2089_i < frg2089_allItem.length; frg2089_i++) {
+        let frg2089_raw = frg2089_allItem[frg2089_i].innerHTML;
 
-    const ifrs = dom.getElementsByTagName("iframe");
-    const frs = dom.getElementsByTagName("frame");
+        let frg2089_data = frg2089_raw.split('<br>');
+        while (frg2089_data.length >= 4) {
+            let frg2089_name;
+            while (frg2089_name == 'undefined' || !frg2089_name) frg2089_name = frg2089_data.shift();
+            frg2089_courses += '{"name":"' + frg2089_name + '",';
+            frg2089_courses += '"time":"' + frg2089_data.shift() + '",';
+            frg2089_courses += '"teacher":"' + frg2089_data.shift() + '",';
+            frg2089_courses += '"place":"' + frg2089_data.shift() + '"},';
 
-    if (ifrs.length) {
-        for (let i = 0; i < ifrs.length; i++) {
-            const dom = ifrs[i].contentWindow.document;
-            iframeContent += scheduleHtmlProvider(iframeContent, frameContent, dom);
+            if (frg2089_data.length >= 7) {
+                frg2089_data.shift();
+                frg2089_data.shift();
+                frg2089_data.shift();
+            } else
+                break;
         }
+
     }
-    if (frs.length) {
-        for (let i = 0; i < frs.length; i++) {
-            const dom = frs[i].contentDocument.body.parentElement;
-            frameContent += scheduleHtmlProvider(iframeContent, frameContent, dom);
-        }
-    }
-    if (!ifrs.length && !frs.length) {
-        return dom.querySelector('body').outerHTML
-    }
-    return dom.getElementsByTagName('html')[0].innerHTML + iframeContent + frameContent
+
+    return ('<div>[' + frg2089_courses + ']</div>').replace('},]', '}]');
 }
